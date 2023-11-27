769a1a4e5b22337dc91e50deb548b85696960580
Lmam22 <laura_aristizabal_manjarres@mckinsey.com>
2023-11-27 14:16:28 -0500

    feat(create-document-db): first version of aws documentdb with aws secrets manager

--- modules/document_db/cluster.tf
+++ modules/document_db/cluster.tf
@@ -1,7 +1,7 @@
 resource "aws_docdb_cluster" "docdb" {
   #CONFIGURATION
   cluster_identifier = "my-docdb-cluster"
-  engine             = "docdb" # The name of the database engine to be used for this DB cluster. Defaults to docdb. Valid Values: docdb
+  engine             = "docdb"
 
   #AUTHENTICATION
   master_username = "root"
@@ -19,9 +19,8 @@ resource "aws_docdb_cluster" "docdb" {
   storage_encrypted = true
 
   #BACKUP
-  backup_retention_period = 5             #The days to retain backups for. Default 1
-  preferred_backup_window = "07:00-09:00" #The daily time range during which automated backups are created if automated backups are enabled using the BackupRetentionPeriod parameter.Time in UTC Default: A 30-minute window selected at random from an 8-hour block of time per regionE.g., 04:00-09:00
-  apply_immediately       = false
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
 
   #TAGS
   tags = {
@@ -29,9 +28,7 @@ resource "aws_docdb_cluster" "docdb" {
   }
 
   #DELETION PROTECTION
-  # final_snapshot_identifier - (Optional) The name of your final DB snapshot when this DB cluster is deleted. If omitted, no final snapshot will be made.
-  skip_final_snapshot = true #Determines whether a final DB snapshot is created before the DB cluster is deleted. If true is specified, no DB snapshot is created. If false is specified, a DB snapshot is created before the DB cluster is deleted, using the value from final_snapshot_identifier. Default is false.
-
+  skip_final_snapshot = true
 }
 
 resource "aws_security_group" "vpc_documentdb_sg" {
