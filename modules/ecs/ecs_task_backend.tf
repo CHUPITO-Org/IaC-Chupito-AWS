de6f4d4ca145c494bab303ba3cf6f4163e629e57
Lmam22 <laura_aristizabal_manjarres@mckinsey.com>
2023-12-05 21:40:50 -0500

    feat(deploy-backend): reorder and rename .tfs files

--- modules/ecs/ecs_task_backend.tf
+++ modules/ecs/ecs_task_backend.tf
@@ -51,26 +51,3 @@ DEFINITION
     Project = "chupito"
   }
 }
-
-resource "aws_security_group" "internal_sg_task" {
-  name   = "internal-task-security-group"
-  vpc_id = var.vpc_id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = 5002
-    to_port         = 5002
-    security_groups = [aws_security_group.lb-internal.id]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Project = "chupito"
-  }
-}
--- modules/ecs/ecs_task_frontend.tf
+++ modules/ecs/ecs_task_frontend.tf
@@ -32,26 +32,3 @@ DEFINITION
     Project = "chupito"
   }
 }
-
-resource "aws_security_group" "sg_task" {
-  name   = "task-security-group"
-  vpc_id = var.vpc_id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = 80
-    to_port         = 80
-    security_groups = [aws_security_group.lb.id]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Project = "chupito"
-  }
-}
